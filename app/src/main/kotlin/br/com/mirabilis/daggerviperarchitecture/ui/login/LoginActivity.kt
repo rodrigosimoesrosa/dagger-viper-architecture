f1c03450505a151d7eb0f9e567508ddb1c7e3f90
Rodrigo Simões Rosa <rodrigosimoesrosa@gmail.com>
2019-06-05 13:55:16 -0300

    Add login activity

--- app/src/main/kotlin/br/com/mirabilis/daggerviperarchitecture/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/br/com/mirabilis/daggerviperarchitecture/ui/login/LoginActivity.kt
@@ -1,6 +1,5 @@
 package br.com.mirabilis.daggerviperarchitecture.ui.login
 
-import android.os.Bundle
 import br.com.mirabilis.daggerviperarchitecture.R
 import br.com.mirabilis.daggerviperarchitecture.base.ui.MVPBaseActivity
 import br.com.mirabilis.daggerviperarchitecture.presenter.login.Login
@@ -12,10 +11,5 @@ import br.com.mirabilis.daggerviperarchitecture.presenter.login.Login
 class LoginActivity : MVPBaseActivity<Login.View, Login.Presenter>(), Login.View {
 
     override fun getLayout(): Int = R.layout.activity_login
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
 }
